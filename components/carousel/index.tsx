7cde30a52a943a2e0c9168d33a89a00af232281e
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-04 13:15:43 -0800

    refactor(carousel): replace carousel placeholder with actual component

---
+++ components/carousel/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import styles from './carousel.module.scss'
+
+const Carousel: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      Carousel
+    </div>
+  )
+}
+
+export default Carousel;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Carousel from '../components/carousel';
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +11,8 @@ const Home: NextPage = () => {
         <title>Planetary</title>
       </Head>
       <h1 className={styles.title}>Planetary</h1>
-      <div className={styles.carousel_container}>Carousel</div>
+      {/* <div className={styles.carousel_container}>Carousel</div> */}
+      <Carousel />
     </div>
   )
 }
