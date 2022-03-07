719a149604ec84cc1f5166f46bb44818dd1914a9
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-06 23:48:15 -0800

    chore(carousel): add temp styles to carousel card holders

--- components/carousel/index.tsx
+++ components/carousel/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
 import styles from './carousel.module.scss'
+import ReactCardCarousel from 'react-card-carousel'
 
 const Carousel: NextPage = () => {
   return (
     <div className={styles.container}>
-      Carousel
+      {/* Carousel */}
+      <ReactCardCarousel className={styles.carousel}>
+        <div className={styles.card}>First Card</div>
+        <div className={styles.card}>Second Card</div>
+        <div className={styles.card}>Third Card</div>
+      </ReactCardCarousel>
     </div>
   )
 }
