21251db430a712b8b5d9ba6608f4c8cc6d5fbb60
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-10 22:13:52 -0800

    chore(menuBar): add temp component elements and styles

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,14 +1,18 @@
 import styles from './header.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars} from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import MenuBar from '../menuBar'
 
 const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(true);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Planetary</h1>
       <button className={styles.menu_button}>
         <FontAwesomeIcon icon={faBars} className={styles.bar_icon}/>
       </button>
+      {menuIsOpen? <MenuBar /> : <></>}
     </div>
   )
 }
--- components/menuBar/index.tsx
+++ components/menuBar/index.tsx
@@ -2,8 +2,8 @@ import styles from './menuBar.module.scss'
 
 const MenuBar = () => {
   return (
-    <div className={styles.container}>
-      <div>MenuBar</div>
+    <div className={styles.overlay}>
+      <div className={styles.menu_bar}>MenuBar</div>
     </div>
   )
 }
