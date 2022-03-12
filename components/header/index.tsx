ae150faf88098c86cac9c83fea5a4ecdd4dda340
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-11 17:52:46 -0800

    feat(menuBar): add open and close functionality

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,14 +5,14 @@ import { useState } from 'react'
 import MenuBar from '../menuBar'
 
 const Header = () => {
-  const [menuIsOpen, setMenuIsOpen] = useState(true);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Planetary</h1>
-      <button className={styles.menu_button}>
+      <button className={styles.menu_button} onClick={()=>setMenuIsOpen(true)}>
         <FontAwesomeIcon icon={faBars} className={styles.bar_icon}/>
       </button>
-      {menuIsOpen? <MenuBar /> : <></>}
+      {menuIsOpen? <MenuBar closeMenuBar={()=>setMenuIsOpen(false)}/> : <></>}
     </div>
   )
 }
--- components/menuBar/index.tsx
+++ components/menuBar/index.tsx
@@ -1,8 +1,13 @@
 import styles from './menuBar.module.scss'
 
-const MenuBar = () => {
+interface MenuBarProps {
+  closeMenuBar: Function,
+}
+
+const MenuBar = ({ closeMenuBar } : MenuBarProps) => {
   return (
-    <div className={styles.overlay}>
+    <div className={styles.container}>
+      <div className={styles.exit_area} onClick={()=>closeMenuBar()}>TapOut</div>
       <div className={styles.menu_bar}>MenuBar</div>
     </div>
   )
