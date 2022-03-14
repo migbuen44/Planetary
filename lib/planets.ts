6b9d6166f133c8ec7018836404dfcaa2f875bf1e
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-14 15:48:39 -0700

    refactor(lib): delete getSortedMenuBarData function

--- lib/planets.ts
+++ lib/planets.ts
@@ -25,10 +25,3 @@ export const getSortedPlanetCardData = () => {
 
   return planetFilesContentSorted;
 }
-
-export const getSortedMenuBarData = () => {
-  const cardData = getSortedPlanetCardData();
-  const sortedMenuBarData = cardData.map(data => data.name);
-
-  return sortedMenuBarData;
-}
