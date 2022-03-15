2fb3eaef53d3f68c0b1eff6954c93551bfec51aa
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-14 21:43:48 -0700

    style(menuBar): add styles to planet links in menubar

--- components/menuBar/index.tsx
+++ components/menuBar/index.tsx
@@ -9,7 +9,18 @@ const MenuBar = ({ closeMenuBar, menuBarData } : MenuBarProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.exit_area} onClick={()=>closeMenuBar()}>TapOut</div>
-      <div className={styles.menu_bar}>MenuBar</div>
+      <div className={styles.menu_bar}>
+        <div className={styles.menu_bar_title}>
+          <h2>All Planets</h2>
+        </div>
+        <div className={styles.planet_names_container}>
+          {menuBarData.map(planet =>
+            <div className={styles.planet_name} key={planet}>
+              <div>{planet}</div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
