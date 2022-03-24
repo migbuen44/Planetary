16eb05c6019bfb377b2a128bd8f092295947d3d8
Miguel Buenviaje <migbuen44@gmail.com>
2022-03-24 12:55:44 -0700

    chore(planetCard): add images to corresponding planet card with basic styles

--- components/carousel/planetCard.tsx
+++ components/carousel/planetCard.tsx
@@ -1,4 +1,5 @@
 import styles from './carousel.module.scss'
+import Image from 'next/image'
 import type { PlanetCardData } from '../../types/planets'
 import PlanetInfo from './planetInfo';
 
@@ -7,18 +8,25 @@ interface PlanetCardProps {
   isCard: boolean,
 }
 
+const imageDirectory = '/images/planet_card_images'
+
 const PlanetCard = ({ planet, isCard } : PlanetCardProps) => {
   const {
     name,
   } = planet;
 
-  if(isCard) return(
+  if(isCard) return( // returns component with styles for home page component card
     <div className={styles.card}>
+      <div className={styles.image_container}>
+        <div className={styles.image_wrapper}>
+          <Image src={`${imageDirectory}/${name}.png`} layout="fill" objectFit="contain"/>
+        </div>
+      </div>
       <PlanetInfo planet={planet} isCard/>
     </div>
   )
 
-  return (
+  return ( // returns component with styles for dynamc route page component card info section
     <div className={styles.card_on_page}>
       <PlanetInfo planet={planet} isCard={false}/>
     </div>
